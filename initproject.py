53f44231323a2378a0361b7b96d0655b6a8c6246
Victoria <vmoix001@mail.ru>
2019-09-28 09:41:58 +0200

    done

--- initproject.py
+++ initproject.py
@@ -1,16 +1,16 @@
 # Developer: Moiseenko V.
-# To count: the weight of U235 (kg), the weight of deuterium (kg), the cost of U235 ($)
-#           the weight of U238 (kg), the cost of U238 ($), the weight of U235 after the half period of time
+# To count: the weight of U235(kg), the weight of deuterium(kg), the cost of U235($)
+#           the weight of U238(kg), the cost of U238($), the weight of U235 after the half period of time(kg)
 daily_energy_take_mvt = int(input())
-daily_energy_take_kvt = daily_energy_take_mvt / (1000)
+population = int(input())
+time = int(input())
+daily_energy_take_kvt = daily_energy_take_mvt / (10**3)
 weight_u235_kg = daily_energy_take_kvt / 20
 weight_deuterium_kg = daily_energy_take_kvt / 150
 cost_u235 = weight_u235_kg * 2360
 weight_u238_kg = (weight_u235_kg / 0.7) * 100
 cost_u238 = weight_u238_kg * 17.3
-population = int(input())
 costs_per_person = cost_u238 / population
-time = int(input())
 weight_u235_half_life = weight_u235_kg * (2**(-((time)/ 70000000)))
 print("Масса U235:", weight_u235_kg)
 print("Масса U238:", weight_u238_kg)
